e2cc948c6967fdbf2873934335b2cc39c797b694
mufasa-349 <76789838+mufasa-349@users.noreply.github.com>
2025-11-04 20:32:01 +0300

    register page created

---
+++ src/page/register.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../component/Common/Header'
+import Banner from '../component/Common/Banner'
+import RegisterArea from '../component/Register'
+import Footer from '../component/Common/Footer'
+
+const Register = () => {
+    return (
+        <>
+            <Header />
+            <Banner title="Kayıt Ol" />
+            <RegisterArea />
+            <Footer />
+        </>
+    )
+}
+
+export default Register
